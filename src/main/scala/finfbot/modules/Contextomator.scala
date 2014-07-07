2110a635f834e184f0aa8cbea23a137403ad52e3
SammysHP <sven@sammyshp.de>
2014-07-07 12:47:15 +0200

    Ricklink in PN

--- src/main/scala/finfbot/modules/Contextomator.scala
+++ src/main/scala/finfbot/modules/Contextomator.scala
@@ -2,10 +2,9 @@ import java.net.{HttpURLConnection, URL}
 
 import org.pircbotx.PircBotX
 import org.pircbotx.hooks.ListenerAdapter
-import org.pircbotx.hooks.events.MessageEvent
+import org.pircbotx.hooks.events.{MessageEvent, PrivateMessageEvent}
 
 object Contextomator extends ListenerAdapter[PircBotX] {
-  type Message = MessageEvent[PircBotX]
 
   def getRicklink(): Option[String] = {
     try {
@@ -18,9 +17,30 @@ object Contextomator extends ListenerAdapter[PircBotX] {
     }
   }
 
-  override def onMessage(event: Message): Unit = {
-    if (event.getMessage.toLowerCase contains "ricklink") {
+  override def onMessage(event: MessageEvent[PircBotX]): Unit = {
+    val message = event.getMessage.toLowerCase
+    val user = event.getUser.getNick
+
+    // Direct interaction with the bot
+    if (message startsWith event.getBot.getNick.toLowerCase) {
+      // Default fallback
+      event.getChannel.send.message("I'm sorry " + user + ", I'm afraid I can't do that...")
+      return
+    }
+
+    // Generic context sensitive actions
+    if (message contains "ricklink") {
       getRicklink foreach(link => event.getChannel.send.message(link))
+    }
+  }
+
+  override def onPrivateMessage(event: PrivateMessageEvent[PircBotX]): Unit = {
+    val message = event.getMessage.toLowerCase
+    val user = event.getUser.getNick
+
+    // Generic context sensitive actions
+    if (message contains "ricklink") {
+      getRicklink foreach(link => event.respond(link))
     }
   }
 }
